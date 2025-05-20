dcd6ead8e9043fd89577844aa33a4643f955668f
baha5255 <baha.deve@gmail.com>
2025-05-20 10:38:05 +0500

    third

--- eslint.config.js
+++ eslint.config.js
@@ -6,14 +6,13 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfig([
   {
-    publicPath:`/JobInbank/`,
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  
+
 
   {
     languageOptions: {
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JobInBank/',
   server: {
     allowedHosts: ['18af-162-120-21-66.ngrok-free.app']
   },
